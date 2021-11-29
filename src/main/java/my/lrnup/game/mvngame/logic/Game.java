4b180bb72b263419d7ffaae7c50a71cf0821ff35
rovnanov <arpacc@yandex.ru>
2021-11-29 15:50:16 +0300

    Initial commit

--- src/main/java/my/lrnup/game/mvngame/logic/Game.java
+++ src/main/java/my/lrnup/game/mvngame/logic/Game.java
@@ -1,25 +1,20 @@
 package my.lrnup.game.mvngame.logic;
 
 public class Game {
-    public static void main(String[] args) {
-        Settings settings = new Settings(false, 10);
-        String[] nameSpeed = {"Bob 0", "Jack -11", "Tolyan 10", "Mikhalych 11", "Stepa 50", "VTB 100"};
-        int[] speeds = {0, -11, 10, 11, 50, 100};
-        System.out.println("Количество выбывших игроков = " + settings.outCount(speeds));
-        System.out.println("Массив из имен выбывающих:");
-        String[] loseNames = settings.nameArray(nameSpeed);
-        for (int i = 0; i < loseNames.length; i++) {
-            System.out.println(loseNames[i]);
+    public boolean isGreen;
+    public int speed;
+
+    public boolean getIsGreen() {
+        return isGreen;
+    }
+    public void setIsGreen(boolean isGreen) {
+       this.isGreen = isGreen;
+    }
+    public boolean isFailed(int speed){
+        SpeedyGame speedyGame = new SpeedyGame(false,10);
+        if(!isGreen){
+            if(speed > speedyGame.maxSpeed) return true;
         }
-        System.out.println("Массив из скоростей выбывающих:");
-        int[] lose = settings.loseArray(speeds);
-        for (int i = 0; i < lose.length; i++) {
-            System.out.println(lose[i]);
-        }
-        System.out.println("Массив из скоростей не выбывающих:");
-        int[] won = settings.wonArray(speeds);
-        for (int i = 0; i < won.length; i++) {
-            System.out.println(won[i]);
-        }
+        return false;
     }
 }
--- src/test/java/my/lrnup/game/mvngame/logic/GameTest.java
+++ src/test/java/my/lrnup/game/mvngame/logic/GameTest.java
@@ -5,88 +5,23 @@ import org.junit.jupiter.api.Test;
 
 public class GameTest {
     @Test
-    public void shouldCountOutPlayersWhenLightIsGreen() {
-        Settings settings = new Settings(true, 10);
-        int[] speed = {1, 2, 3, 4, 5};
+    public void PlayerLoseTest(){
+        SpeedyGame speedyGame = new SpeedyGame(false, 10);
+        Game game = new Game();
+        int speed = 11;
 
-        int expected = 0;
-        int actual = settings.outCount(speed);
-
-        Assertions.assertEquals(expected, actual, "Метод outCount работает корректно при зелёном цвете");
+        boolean expected = true;
+        boolean actual = game.isFailed(speed);
+        Assertions.assertTrue(actual,"Игрок проиграл");
     }
-
     @Test
-    public void shouldCountOutPlayersWhenLightIsNotGreen() {
-        Settings settings = new Settings(false, 10);
-        int[] speed = {0, -11, 10, 11, 50, 100};
+    public void PlayerWonTest(){
+        SpeedyGame speedyGame = new SpeedyGame(false, 10);
+        Game game = new Game();
+        int speed = 10;
 
-        int expected = 3;
-        int actual = settings.outCount(speed);
-
-        Assertions.assertEquals(expected, actual, "Метод outCount работает корректно при красном свете");
-    }
-
-    @Test
-    public void shouldCalcLoseArrayWhenLightIsGreen() {
-        Settings settings = new Settings(true, 10);
-        int[] speed = {0, -11, 10, 11, 50, 100};
-
-        int[] expected = {};
-        int[] actual = settings.loseArray(speed);
-
-        Assertions.assertArrayEquals(expected, actual, "Метод loseArray работает корректно при зеленом свете");
-    }
-
-    @Test
-    public void shouldCalcLoseArrayWhenLightIsNotGreen() {
-        Settings settings = new Settings(false, 10);
-        int[] speed = {0, -11, 10, 11, 50, 100};
-
-        int[] expected = {11, 50, 100};
-        int[] actual = settings.loseArray(speed);
-
-        Assertions.assertArrayEquals(expected, actual, "Метод loseArray работает корректно при красном свете");
-    }
-
-    @Test
-    public void shouldCalcWonArrayWhenLightIsGreen() {
-        Settings settings = new Settings(true, 10);
-        int[] speed = {0, -11, 10, 11, 50, 100};
-
-        int[] expected = {0, -11, 10, 11, 50, 100};
-        int[] actual = settings.wonArray(speed);
-
-        Assertions.assertArrayEquals(expected, actual, "Метод wonArray работает корректно при зеленом свете");
-    }
-
-    @Test
-    public void shouldCalcWonArrayWhenLightIsNotGreen() {
-        Settings settings = new Settings(false, 10);
-        int[] speed = {0, -11, 10, 11, 50, 100};
-
-        int[] expected = {0, -11, 10};
-        int[] actual = settings.wonArray(speed);
-
-        Assertions.assertArrayEquals(expected, actual, "Метод wonArray работает корректно при красном свете");
-    }
-
-    @Test
-    public void nameArrayWorksWithGreenLight() {
-        Settings settings = new Settings(true, 10);
-        String[] speed = {"Собака 10", "Кошка 0"};
-
-        String[] expected = {};
-        String[] actual = settings.nameArray(speed);
-        Assertions.assertArrayEquals(expected, actual, "Метод nameArray работает корректно при зеленом свете");
-    }
-
-    @Test
-    public void nameArrayWorksWithRedLight() {
-        Settings settings = new Settings(false, 10);
-        String[] speed = {"Собака 11", "Кошка 0"};
-
-        String[] expected = {"Кошка"};
-        String[] actual = settings.nameArray(speed);
-        Assertions.assertArrayEquals(expected, actual, "Метод nameArray работает корректно при красном свете");
+        boolean expected = false;
+        boolean actual = game.isFailed(speed);
+        Assertions.assertFalse(actual, "Игрок выйграл");
     }
 }
